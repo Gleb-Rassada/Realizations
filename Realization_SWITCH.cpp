e43feedd5b0ef24b06ca395f768eab7031734e35
Gleb-Rassada <79448263+Gleb-Rassada@users.noreply.github.com>
2021-03-30 08:48:43 +0300

    Add files via upload

---
+++ Realization_SWITCH.cpp
@@ -0,0 +1,42 @@
+﻿//Реализация switch
+
+#include <iostream>
+using namespace std;
+
+int CorrectNumber(string x) {
+    int n = 0;
+    while (true)
+    {
+        cout << x;
+        cin >> n;
+        if (!cin)
+        {
+            cout << "Ошибка ввода!\n";
+            cin.clear();
+            while (cin.get() != '\n');
+        }
+        else break;
+    }
+    return n;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int n = CorrectNumber("Введите 1 или 2 или 3:\n");
+
+    switch (n) {
+    case 1:
+        cout << "Поздравляю у вас девочка";
+        break;
+    case 2:
+        cout << "Поздравляю у вас мальчик";
+        break;
+    case 3:
+        cout << "Поздравляю у вас ATTACK HELICOPTER!!!";
+    default:
+        cout << "nothing";
+    }
+
+}
